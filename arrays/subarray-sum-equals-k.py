a396889cc272fb007f638c6e6ed578d80077c5d0
Jaden Padua <46204970+jadenpadua@users.noreply.github.com>
2021-09-03 12:12:00 -0700

    Update subarray-sum-equals-k.py

--- arrays/subarray-sum-equals-k.py
+++ arrays/subarray-sum-equals-k.py
@@ -1,7 +1,9 @@
 # Using [3,4,7,2,-3,1,4,2], k = 7
+from collections import defaultdict
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
-        ht = {0:1}
+        ht = defaultdict(lambda: 0)
+        ht[0] = 1
         runningSum = 0
         count = 0
         for i in range(len(nums)):
@@ -11,6 +13,6 @@ class Solution:
             if prevSumToFind in ht:
                 count += ht[prevSumToFind]
                 
-            ht[runningSum] = ht.get(runningSum, 0) + 1
+            ht[runningSum] += 1
             
         return count
